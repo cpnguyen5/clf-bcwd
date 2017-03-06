9a5b0fc6019ad1051c5dbe9426a725101d7686ba
cpnguyen <cpnguyen5@dons.usfca.edu>
2017-03-06 00:03:13 -0800

    Create readCSV func

--- src/clf.py
+++ src/clf.py
@@ -1,11 +1,13 @@
 import os, sys
+import numpy as np
+import pandas as pd
 
 
 def getPath():
     """
     Function takes no parameters, returning the pathway of the data csv file.
 
-    :return: directory pathway of csv file
+    :return: directory pathway of CSV file
     """
     abspath = os.path.abspath(__file__) # absolute pathway to file
     head_path, f_name = os.path.split(abspath)
@@ -13,3 +15,24 @@ def getPath():
     csvpath = os.path.join(work_dir, 'data', 'breast-cancer-wisconsin.csv')
     return csvpath
 
+
+def readCSV(path):
+    """
+    Function returns the contents of the csv data as a pandas DataFrame.
+
+    :param path: pathway to CSV file
+    :return: pandas DataFrame
+    """
+    cols = ['id', 'clump_thickness', 'size_uniformity', 'shape_uniformity', 'adhesion',
+             'epithelial_size', 'bare_nuclei', 'bland_chromatin', 'normal_nucleoli', 'mitoses', 'class']
+
+    df = pd.read_csv(path, sep=',', header=None, names = cols, na_values='?')
+    df = df.dropna() # drop NaN values
+    df.replace(to_replace={'class': {2:0, 4:1}}, inplace=True) # binary classes
+    return df
+
+
+#main
+if __name__ == '__main__':
+    path = getPath()
+    data = readCSV(path)
